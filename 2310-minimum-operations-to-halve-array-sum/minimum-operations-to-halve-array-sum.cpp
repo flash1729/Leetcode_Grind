c337b706fb4b9743e88d46a9111c0f17662d1ec0
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-02-13 18:38:01 +0530

    Time: 71 ms (97.44%) | Memory: 85.2 MB (87.20%) - LeetSync

--- 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
@@ -1,26 +1,45 @@
+// class Solution {
+// public:
+//     int halveArray(vector<int>& nums) {
+//         double sum = 0;
+//         int ops = 0;
+//         priority_queue<double> res;
+//         for(int i = 0;i < nums.size();i++){
+//             sum += nums[i];
+//             res.push(nums[i]);
+//         }
+//         double temp = sum;
+//         cout<<"temp = "<<temp<<endl;
+
+//         while(sum > temp / 2){
+//             ops++;
+//             double popEle = res.top();
+//             res.pop();
+//             sum -= popEle / 2;
+//             popEle /= 2;
+//             res.push(popEle);
+//         }
+
+//         return ops;
+//     }
+// };
+
 class Solution {
 public:
     int halveArray(vector<int>& nums) {
-        double sum = 0;
-        int ops = 0;
-        priority_queue<double> res;
-        for(int i = 0;i < nums.size();i++){
-            sum += nums[i];
-            res.push(nums[i]);
+        int ops {0};
+        double sum {0};
+        for(int num : nums) { sum += num; }
+        double half_sum = (double)sum / 2;
+        priority_queue<double> pq(nums.begin(), nums.end());
+        while(sum > half_sum) {
+            double num = pq.top(); pq.pop();
+            sum -= num;
+            double half = num / 2;
+            sum += half;
+            pq.push(half);
+            ++ops;
         }
-        double temp = sum;
-        cout<<"temp = "<<temp<<endl;
-
-        while(sum > temp / 2){
-            ops++;
-            double popEle = res.top();
-            res.pop();
-            sum -= popEle;
-            popEle /= 2;
-            res.push(popEle);
-            sum += popEle;
-        }
-
         return ops;
     }
 };
