b2dd2e5500a29abed61c20921bdbc9294d2191e0
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-06-21 11:52:26 +0530

    Time: 11 ms (49.61%) | Memory: 11 MB (57.02%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -7,19 +7,12 @@ public:
         int res = 0;
 
         for(int j = 0;j < s.size();j++){
-            cout<<s[j] - 'a'<<endl;
             counts[s[j] - 'a']++;
-
-            if(counts[0] > 0 && counts[1] > 0 && counts[2] > 0){
-                res += s.size() - j;
-            }
             
             while(counts[0] > 0 && counts[1] > 0 && counts[2] > 0){
+                res += s.length() - j;
                 counts[s[i] - 'a']--;
                 i++;
-                if(counts[0] >= 1 && counts[1] >= 1 && counts[2] >= 1){
-                    res += s.size() - j;
-                }
             }
         }
         
