9a01300a68cfb5ca31bad5215bf349e734819ca5
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-06-26 12:14:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (2.69%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -2,57 +2,42 @@ class MyStack {
 public:
     queue<int> q1;
     queue<int> q2;
-
     MyStack() {
+        
     }
     
     void push(int x) {
-        if(q1.empty()){
-            // push x and then whole q2 to it
-            q1.push(x);
-
-            while(!q2.empty()){
-                int t = q2.front();
-                q1.push(t);
-                q2.pop();
-            }
-        }else if(q2.empty()){
-            q2.push(x);
-
-            while(!q1.empty()){
-                int t = q1.front();
-                q2.push(t);
-                q1.pop();
-            }
-        }
+        q1.push(x);
     }
     
     int pop() {
-        if(!q1.empty()){
+        
+        while(q1.size() > 1){
             int t = q1.front();
+            q2.push(t);
             q1.pop();
-            return t;
-        }else if(!q2.empty()){
-            int t = q2.front();
-            q2.pop();
-            return t;
         }
+        
+        int t = q1.front();
+        q1.pop();
+        q1.swap(q2);
 
-        return 0;
+        return t;
     }
     
     int top() {
-        if(!q1.empty()){
+        while(q1.size() > 1){
             int t = q1.front();
-            
-            return t;
-        }else if(!q2.empty()){
-            int t = q2.front();
-            
-            return t;
+            q2.push(t);
+            q1.pop();
         }
 
-        return 0;
+        int t = q1.front();
+        q2.push(t);
+        q1.pop();
+        q1.swap(q2);
+
+        return t;
     }
     
     bool empty() {
