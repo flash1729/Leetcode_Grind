2faff08568c8366a6cc72605f4f5f82a8a07a766
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-07-07 12:23:38 +0530

    Time: 51 ms (57.66%) | Memory: 90.9 MB (31.18%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int , int> freq;
-        for(int i = 0;i<nums.size();i++){
-            freq[nums[i]]++;
-            if(freq[nums[i]] == 2){
+        unordered_map<int,int> freq;
+        
+        for(int a : nums){
+            freq[a]++;
+            if(freq[a] == 2)
                 return true;
-            }
         }
+
         return false;
     }
 };
