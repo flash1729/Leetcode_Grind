2624fce6a0ff329189adfb31722779ccff7485bd
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-07-15 19:03:36 +0530

    Time: 4 ms (1.56%) | Memory: 17.8 MB (89.36%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size() - 1;
+        
+        while(i <= j){
+            while(i < nums.size() - 1 && nums[i] == nums[i + 1]) i++;
+            while(j > 0 && nums[j] == nums[j - 1]) j--;
+
+            int mid = i + (j-i)/2;
+            cout<<mid<<" "<<nums[mid]<<" "<<i<<" "<<j<<endl;
+
+            // if(nums[i] == target) return i;
+            // if(nums[j] == target) return j;
+
+            if(nums[mid] == target){
+                return true;
+            }
+            else if(nums[i] <= nums[mid]){
+                if(target >= nums[i] && target <= nums[mid]){
+                    j = mid - 1;
+                }else{
+                    i = mid + 1;
+                }
+            }
+            else{
+                if(target <= nums[j] && target >= nums[mid]){
+                    i = mid + 1;
+                }else{
+                    j = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
