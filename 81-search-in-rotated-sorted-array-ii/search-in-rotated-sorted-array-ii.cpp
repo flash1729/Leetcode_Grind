38c8032706f849f78875cf22695c6f3c4be0551b
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-07-15 19:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (20.08%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -5,29 +5,28 @@ public:
         int j = nums.size() - 1;
         
         while(i <= j){
-            while(i < nums.size() - 1 && nums[i] == nums[i + 1]) i++;
-            while(j > 0 && nums[j] == nums[j - 1]) j--;
+    int mid = i + (j-i)/2;
+    if(nums[mid] == target) return true;
 
-            int mid = i + (j-i)/2;
-
-            if(nums[mid] == target){
-                return true;
-            }
-            else if(nums[i] <= nums[mid]){
-                if(target >= nums[i] && target <= nums[mid]){
-                    j = mid - 1;
-                }else{
-                    i = mid + 1;
-                }
-            }
-            else{
-                if(target <= nums[j] && target >= nums[mid]){
-                    i = mid + 1;
-                }else{
-                    j = mid - 1;
-                }
-            }
+    // duplicates shrink
+    if(nums[i] == nums[mid] && nums[mid] == nums[j]){
+        i++; j--;
+    }
+    else if(nums[i] <= nums[mid]){
+        if(nums[i] <= target && target < nums[mid]){
+            j = mid - 1;
+        }else{
+            i = mid + 1;
         }
+    }else{
+        if(nums[mid] < target && target <= nums[j]){
+            i = mid + 1;
+        }else{
+            j = mid - 1;
+        }
+    }
+}
+
 
         return false;
     }
