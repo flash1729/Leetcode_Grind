fbb25dd326b98e14a5fd73356106c2940abb69c1
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-07-27 21:09:59 +0530

    Time: 3 ms (30.74%) | Memory: 100.3 MB (90.81%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        if(nums.size() == 1) return true;
+        int i = 1;
+        while(i < nums.size() - 1 && nums[i] == nums[i - 1]) i++;
+
+        if(nums[i] > nums[i - 1]){
+            for(; i < nums.size(); i++){
+                if(nums[i] < nums[i-1]) return false;
+            }
+        }else{
+            for(; i < nums.size(); i++){
+                if(nums[i] > nums[i-1]) return false;
+            }
+        }
+
+        return true;
+    }
+};
