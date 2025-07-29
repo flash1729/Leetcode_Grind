fe4b2b0ea6e8666ed4551838ed1a6219cbca8216
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-07-29 08:07:10 +0530

    Time: 17 ms (5.14%) | Memory: 12.8 MB (61.13%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> myStack;
+        vector<int> res;
+
+        stack<int> ind;
+        int n2 = nums2.size();
         unordered_map<int,int> ng;
-        vector<int> ans;
-        for(int i = 0;i < nums2.size();i++){
-            while(!myStack.empty()){
-                if(myStack.top() < nums2[i]){
-                    ng[myStack.top()] = nums2[i];
-                    myStack.pop();
-                }
-                else{
-                    break;
-                }
+
+
+        for(int i = n2 - 1 ; i >= 0; i--){
+            while(!ind.empty() && nums2[ind.top()] <= nums2[i]){
+                ind.pop();
             }
-            myStack.push(nums2[i]);
-        }
-        while(!myStack.empty()){
-            ng[myStack.top()] = -1;
-            myStack.pop();
+
+            if(ind.empty()){
+                ng[nums2[i]] = -1;
+                cout<<nums2[i]<<" -1"<<endl;
+            }else{
+                ng[nums2[i]] = nums2[ind.top()];
+                cout<<nums2[i]<<" "<<nums2[ind.top()]<<endl;
+            }
+
+            ind.push(i);
         }
 
-        for(int i : nums1){
-            ans.push_back(ng[i]);
+        for(int i = 0;i < nums1.size();i++){
+            res.push_back(ng[nums1[i]]);
         }
 
-        return ans;
+        return res;
     }
 };
