1714db682bd52d58dd0ca6631e4173bd1e36c54c
Aditya Medhane <153186739+flash1729@users.noreply.github.com>
2025-09-01 08:09:15 +0530

    Time: 2140 ms (5.26%) | Memory: 296.8 MB (5.85%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    struct maxP{
+        bool operator()(vector<int> &p, vector<int> &q){
+            double pOld = (double)p[0] / (double)p[1];
+            double qOld = (double)q[0] / (double)q[1];
+
+            double pNew = ((double)p[0] + 1) / ((double)p[1] + 1);
+            double qNew = ((double)q[0] + 1) / ((double)q[1] + 1);
+
+            return (pNew - pOld) < (qNew - qOld);
+        }
+    };
+
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        priority_queue<vector<int>,vector<vector<int>>,maxP> pq;
+        double average = 0;
+
+        for(auto a : classes){
+            pq.push(a);
+            average += (double)a[0] / (double)a[1];
+            cout<<average<<endl;
+        }
+
+        for(int i = 0; i < extraStudents; i++){
+            vector<int> cur = pq.top();
+            pq.pop();
+            average -= (double)cur[0] / (double)cur[1];
+            average += ((double)cur[0] + 1) / ((double)cur[1] + 1);
+            pq.push({cur[0] + 1,cur[1] + 1});
+        }
+
+        return average / classes.size();
+    }
+};
